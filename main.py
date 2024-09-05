8ab6bbc6013f79204c8c00983fe8875cbe2db4ab
AaronTheGenerous <aaron@windowslive.com>
2024-09-05 17:08:24 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+intended to scrape website sony promotion overview and extract relevant data to be represented in a consise gui
+
+start with scraping
+
+TODO:
+- GUI
+- search by product
+- visually appealing and understandable table view or simular
+- clearly indicate conditions for promotions
+"""
+
