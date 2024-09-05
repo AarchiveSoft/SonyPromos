fd48edf68745db43f57f5493f1b5a032b91a092f
AaronTheGenerous <aaron@windowslive.com>
2024-09-05 17:20:55 +0200

    Add database setup for storing promo details
    Introduced sqlite3 to manage a database for storing Sony promotion details and integrated it with necessary schema creation. Enhances data management and supports efficient data retrieval and storage.

--- main.py
+++ main.py
@@ -10,3 +10,30 @@ TODO:
 - clearly indicate conditions for promotions
 """
 
+import sqlite3
+
+from selenium import webdriver
+from selenium.webdriver.common.by import by
+from selenium.webdriver.chrome import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def createDatabase():
+    conn = sqlite3.connect('sonyPromos.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS promos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            art_nr TEXT NOT NULL,
+            model TEXT,
+            promo_details TEXT,
+            start_date DATE,
+            end_date DATE,
+            scrape_date DATE,
+            product_link TEXT,
+            promo_title TEXT,
+            promo_link TEXT
+        )
+    ''')
