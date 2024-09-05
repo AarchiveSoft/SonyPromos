cc5068bb8cef6e3e02bfcabe5cf9fe01c0b36a97
AaronTheGenerous <aaron@windowslive.com>
2024-09-05 17:29:33 +0200

    Add database setup for storing promo details
    Introduced sqlite3 to manage a database for storing Sony promotion details and integrated it with

--- main.py
+++ main.py
@@ -19,7 +19,13 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 def createDatabase():
+    """
+    Create the database and the 'promos' table if it does not exist.
+
+    :return: None
+    """
     conn = sqlite3.connect('sonyPromos.db')
     cursor = conn.cursor()
 
@@ -37,3 +43,31 @@ def createDatabase():
             promo_link TEXT
         )
     ''')
+
+
+def scrape_info():
+    if getattr(sys, "frozen", False):
+        # Running as packaged executable, driver is in same directory
+        base_path = sys._MEIPASS
+    else:
+        # Running as normal script, driver is in parent directory
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        base_path = os.path.dirname(base_path)
+    chromedriver_path = os.path.join(base_path, 'chromedriver.exe')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.path.join(base_path, 'chrome', 'win64-118.0.5993.70', 'chrome-win64',
+                                                  'chrome.exe')
+
+    service = Service(chromedriver_path)
+
+    try:
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        driver.get(r"https://www.graphicart.ch/de/uebersicht-sony-aktionen/")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return
+
+    wait = WebDriverWait(driver, 10)
+
+    # get category links
+
